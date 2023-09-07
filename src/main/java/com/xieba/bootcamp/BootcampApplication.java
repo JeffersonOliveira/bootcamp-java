aec2058645a35302d0f4ab21b67d22f163ec3fea
Jefferson N. S. de Oliveira <jefferson_nunes01@hotmail.com>
2023-09-07 01:24:40 -0300

    Atualização das informações da classe main.

--- src/main/java/com/xieba/bootcamp/BootcampApplication.java
+++ src/main/java/com/xieba/bootcamp/BootcampApplication.java
@@ -10,41 +10,43 @@ public class BootcampApplication {
 		SpringApplication.run(BootcampApplication.class, args);
 
 		Pessoa pessoa = new Pessoa();
-		/*pessoa.nome = "Jefferson";
-		pessoa.telefone = "21976066744";
-		pessoa.documento = "RG";*/
-
 		pessoa.setNome("Jefferson");
-		pessoa.setTelefone("21976066744");
+		pessoa.setTelefone("2197666 6545");
 		pessoa.setDocumento("RG");
-//		pessoa.endereco = "Rua Jardim Das Oliveiras, 417, Casa, Centro";
-//		System.out.println(pessoa);
-//		System.out.println(pessoa.nome);
-//		System.out.println(pessoa.telefone);
-//		System.out.println(pessoa.documento);
-//		System.out.println(pessoa.endereco);
+		/*
+		pessoa.endereco = "Rua Jardim Das Oliveiras, 417, Casa, Centro";
+		System.out.println(pessoa);
+		System.out.println(pessoa.nome);
+		System.out.println(pessoa.telefone);
+		System.out.println(pessoa.documento);
+		System.out.println(pessoa.endereco);
+		*/
 
 		Endereco endereco = new Endereco();
-		endereco.setLogradouro("Rua Carlos Gomes");
+		endereco.setLogradouro("Rua Roberto Carlos");
 		endereco.setComplemento("Casa");
 		endereco.setNumero("100");
-		endereco.setBairro("São geraldo");
-		endereco.setCidade("Nova Iguaçu");
-		endereco.setCep("26276410");
+		endereco.setBairro("Centro");
+		endereco.setCidade("Rio de Janeiro");
+		endereco.setCep("26000000");
 
 		pessoa.setEndereco(endereco);
 
 		System.out.println(pessoa.toString());
-		/*System.out.println("pessoa.nome: "+ pessoa.getNome());
+		/*
+		System.out.println("pessoa.nome: "+ pessoa.getNome());
 		System.out.println("pessoa.telefone: "+ pessoa.getTelefone());
-		System.out.println("pessoa.doc: "+ pessoa.getDocumento());*/
+		System.out.println("pessoa.doc: "+ pessoa.getDocumento());
+		*/
 		System.out.println("pessoa.end: "+ pessoa.getEndereco());
-		/*System.out.println("pessoa.logra: "+ endereco.getLogradouro());
-		//System.out.println("pessoa.logra: "+ pessoa.endereco.getLogradouro());
+		/*
+		System.out.println("pessoa.logra: "+ endereco.getLogradouro());
+		System.out.println("pessoa.logra: "+ pessoa.endereco.getLogradouro());
 		System.out.println("pessoa.comp: "+ endereco.getComplemento());
 		System.out.println("pessoa.num: "+ endereco.getNumero());
 		System.out.println("pessoa.bai: "+ endereco.getBairro());
 		System.out.println("pessoa.cid: "+ endereco.getCidade());
-		System.out.println("pessoa.cep: "+ endereco.getCep());*/
+		System.out.println("pessoa.cep: "+ endereco.getCep());
+		*/
 	}
 }
