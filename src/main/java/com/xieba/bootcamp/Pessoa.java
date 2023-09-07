53c79f86ba42fc418e776205abdcc11f06631412
Jefferson N. S. de Oliveira <jefferson_nunes01@hotmail.com>
2023-09-07 01:23:05 -0300

    Refatoração da Classe Pessoa, removendo os getters, Setters e ToString para utilizando somente a annotation @Data

--- src/main/java/com/xieba/bootcamp/Pessoa.java
+++ src/main/java/com/xieba/bootcamp/Pessoa.java
@@ -1,40 +1,12 @@
 package com.xieba.bootcamp;
+
+import lombok.Data;
+
+@Data
 public class Pessoa {
+
     private String nome;
     private String telefone;
     private String documento;
     private Endereco endereco;
-    public String getNome(){
-        return nome;
-    }
-    public void setNome(String nome){
-        this.nome = nome;
-    }
-    public String getTelefone(){
-        return this.telefone;
-    }
-    public void setTelefone(String telefone){
-        this.telefone = telefone;
-    }
-    public String getDocumento(){
-        return documento;
-    }
-    public void setDocumento(String documento){
-        this.documento = documento;
-    }
-    @Override
-    public String toString() {
-        return "Pessoa{" +
-                "nome='" + nome + '\'' +
-                ", telefone='" + telefone + '\'' +
-                ", documento='" + documento + '\'' +
-                ", endereco=" + endereco +
-                '}';
-    }
-    public Endereco getEndereco(){
-        return endereco;
-    }
-    public void setEndereco(Endereco endereco){
-        this.endereco = endereco;
-    }
 }
